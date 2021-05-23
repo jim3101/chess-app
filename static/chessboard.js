0bb188b49ce826b43ab84ee8e0ce95cfeae84fd0
Jim <jimhoekstra2@gmail.com>
2021-05-23 10:05:03 +0200

    major refactor of move checking code

--- static/chessboard.js
+++ static/chessboard.js
@@ -1,17 +1,20 @@
-import Move from '/static/move.js';
+import getLegalMoves from '/static/legalMoves.js';
 import { chessChars } from '/static/constants.js';
 
 
 export default class Chessboard {
 
-    constructor(squareElements, initialPositions) {
+    constructor(squareElements, initialPositions, turnTextField) {
         this.squareElements = squareElements;
-        this.positions = {};
-        this.setPositions(initialPositions);
-        this.turn = 'white';
+        this.turnTextField = turnTextField;
 
+        this.positions = {};
+        this.turn = 'white';
+        this.setPositions(initialPositions);
+        
         this.legalEnPassant = null;
         this.enPassantTakes = null;
+        this.selectedPieceLegalMoves = null;
     }
 
     setPositions(positionObject) {
@@ -55,12 +58,40 @@ export default class Chessboard {
                 square.appendChild(p);
             }
         })
+        this.turnTextField.innerText = this.turn + "'s turn";
         this.setPieceEventListeners();
     }
 
     setPieceEventListeners() {
         let pieces = document.querySelectorAll('.chessboard-square-piece');
         Array.from(pieces).forEach(piece => {
+            piece.addEventListener('mousedown', () => {
+                const currentPiece = this.positions[piece.id];
+                this.selectedPieceLegalMoves = getLegalMoves(this.turn, currentPiece, piece.id, this.positions, null, true);
+                this.selectedPieceLegalMoves.forEach(legalMove => {
+                    const square = document.getElementById(legalMove.newSquare);
+                    square.classList.add('legal-move');
+                })
+            })
+
+            piece.addEventListener('mouseup', () => {
+                if (this.selectedPieceLegalMoves !== null) {
+                    this.selectedPieceLegalMoves.forEach(legalMove => {
+                        const square = document.getElementById(legalMove.newSquare);
+                        square.classList.remove('legal-move');
+                    })
+                }
+            })
+
+            piece.addEventListener('mouseleave', () => {
+                if (this.selectedPieceLegalMoves !== null) {
+                    this.selectedPieceLegalMoves.forEach(legalMove => {
+                        const square = document.getElementById(legalMove.newSquare);
+                        square.classList.remove('legal-move');
+                    })
+                }
+            })
+
             piece.addEventListener('dragstart', () => {
                 piece.classList.add('dragging');
             })
@@ -92,6 +123,13 @@ export default class Chessboard {
             })
 
             square.addEventListener('drop', (e) => {
+                if (this.selectedPieceLegalMoves !== null) {
+                    this.selectedPieceLegalMoves.forEach(legalMove => {
+                        const square = document.getElementById(legalMove.newSquare);
+                        square.classList.remove('legal-move');
+                    })
+                }
+                
                 square.classList.remove('dragging-over');
                 const movingPieceElement = document.querySelector('.chessboard-square-piece.dragging');
                 
@@ -101,21 +139,16 @@ export default class Chessboard {
                     const currentSquare = movingPieceElement.id;
                     const targetSquare = square.id;
                     const movingPiece = this.positions[currentSquare];
-                    const move = new Move(this.turn, movingPiece, currentSquare, targetSquare);
 
-                    if (move.isLegal(this.positions, this.legalEnPassant)) {
-                        if (targetSquare === this.legalEnPassant) {
-                            delete this.positions[this.enPassantTakes];
-                        }
-                        this.legalEnPassant = move.enablesEnPassant(this.positions);
-                        this.enPassantTakes = move.getEnPassantTakes();
-
+                    if (this.selectedPieceLegalMoves.find(move => move.newSquare === targetSquare)) {
                         delete this.positions[movingPieceElement.id];
                         this.positions[targetSquare] = movingPiece;
-                        this.render();
                         this.nextTurn();
+                        this.render();
                     }
                 }
+
+                this.selectedPieceLegalMoves = null;
             })
         })
     }
--- static/main.js
+++ static/main.js
@@ -2,13 +2,28 @@ import Chessboard from '/static/chessboard.js';
 import { initialPositions } from '/static/constants.js';
 
 
-let squares = document.getElementsByClassName('chessboard-square');
-let initialPositionButton = document.getElementById('initial-position-button');
+const squares = document.getElementsByClassName('chessboard-square');
+const initialPositionButton = document.getElementById('initial-position-button');
+const flipBoardButton = document.getElementById('flip-board-button');
+const turnTextField = document.getElementById('turn-text');
 
-let chessboard = new Chessboard(squares, initialPositions);
+let chessboard = new Chessboard(squares, initialPositions, turnTextField);
 
 
 initialPositionButton.addEventListener('click', () => {
-    chessboard.setPositions(initialPositions);
     chessboard.setTurn('white');
+    chessboard.setPositions(initialPositions);
 });
+
+
+flipBoardButton.addEventListener('click', () => {
+    const chessboardGrid = document.getElementById('chessboard');
+    let chessboardChildren = Array.from(chessboardGrid.children);
+
+    while (chessboardGrid.firstChild !== null) {
+        chessboardGrid.removeChild(chessboardGrid.firstChild);
+    }
+
+    chessboardChildren.reverse();
+    chessboardGrid.append(...chessboardChildren);
+})
